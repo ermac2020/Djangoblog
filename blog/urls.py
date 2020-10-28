ee01169100b3e4e4bb897e483b63723d5b6f8b1e
ermac2020 <ermac@mesoamericana.edu.gt>
2020-10-28 17:54:19 -0600

    tarea

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.post_lista, name='post_lista'),
     path('publicacion/<int:pk>/', views.post_detalle, name='post_detalle'),
+    path('post/nuevo', views.post_nuevo, name='post_nuevo'),
+    path('post/<int:pk>/editar/', views.post_editar, name='post_editar'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import redirect
+from .forms import PostForm
 
 def post_lista(request):
     publicaciones = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -9,3 +11,30 @@ def post_lista(request):
 def post_detalle(request, pk):
     publicaciones = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detalle.html', {'publicaciones': publicaciones})
+
+def post_nuevo(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detalle', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_editar.html', {'form': form})
+
+def post_editar(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detalle', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_editar.html', {'form': form})
